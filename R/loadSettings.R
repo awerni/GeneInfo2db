06a1f80b5f9096b8e72f622da30a76761ea0e336
wernitzn <andreas.wernitznig@boehringer-ingelheim.com>
2021-02-26 22:49:16 +0100

    new default db server

--- R/loadSettings.R
+++ R/loadSettings.R
@@ -1,6 +1,6 @@
 loadSettings <- function() {
   options("dbname" = "bioinfo.hg38")
-  options("dbhost" = "charlotte")
+  options("dbhost" = "vie-bio-postgres")
   options("useFileDownload" = TRUE)
   options("msigdb_path" = "~/Download/")
 }
