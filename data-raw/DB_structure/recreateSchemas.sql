469ae566faadb1d8cfdffbef26bd87f1ce23e1a9
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-03 08:49:31 +0100

    added schema cellline

--- data-raw/DB_structure/recreateSchemas.sql
+++ data-raw/DB_structure/recreateSchemas.sql
@@ -1,5 +1,10 @@
-DROP SCHEMA public CASCADE;
+DROP SCHEMA IF EXISTS cellline CASCADE;
+DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
+CREATE SCHEMA cellline;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
+GRANT ALL ON SCHEMA cellline TO postgres;
+GRANT ALL ON SCHEMA cellline TO public;
 COMMENT ON SCHEMA public IS 'standard public schema';
+COMMENT ON SCHEMA cellline IS 'cellline schema';
