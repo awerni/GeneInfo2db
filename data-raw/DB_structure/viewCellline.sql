02061f4cd951aa9a91aadf7aaef1b271e37d1042
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-07 16:47:49 +0100

    commented out some views

--- data-raw/DB_structure/viewCellline.sql
+++ data-raw/DB_structure/viewCellline.sql
@@ -9,22 +9,13 @@ JOIN cellline.hlatype h ON (n.rnaseqrunid = h.rnaseqrunid AND hla_class = 'A');
 
 -----------------------------------------------------
 
-DROP VIEW IF EXISTS cellline.hybrid;
-CREATE VIEW cellline.hybrid AS
-SELECT hybridizationid, h.chipname, chiptype, h.celllinename, organ, tumortype, histology_type, histology_subtype, 
-       morphology, growth_type, laboratory, hybridizationgroupname, cellbatchid, analysisdirectory as directory, isxenograft, compound, h.comment 
-FROM cellline.hybridization h JOIN cellline.hybridizationgroup hg ON (hg.hybridizationgroupid = h.hybridizationgroupid) 
-JOIN cellline.cellline c ON (h.celllinename = c.celllinename) JOIN chiptechnology ct ON (ct.chipname = h.chipname) WHERE publish = TRUE;
-
--------------------------------------------------
-
 DROP VIEW IF EXISTS cellline.availabledata;
-CREATE VIEW cellline.availabledata AS 
+CREATE VIEW cellline.availabledata AS
 SELECT celllinename,
   exists (SELECT enst FROM cellline.processedsequence WHERE celllinename = cl.celllinename) as dnaseqexists,
-  exists (SELECT ensg FROM cellline.processedcopynumber WHERE celllinename = cl.celllinename) as copynumberexists, 
+  exists (SELECT ensg FROM cellline.processedcopynumber WHERE celllinename = cl.celllinename) as copynumberexists,
   exists (SELECT rnaseqrunid FROM cellline.rnaseqrun WHERE celllinename = cl.celllinename AND canonical) as rnaseqexists,
-  exists (SELECT ensg1 FROM cellline.processedfusiongene WHERE celllinename = cl.celllinename) as fusiongeneexists,       
+  exists (SELECT ensg1 FROM cellline.processedfusiongene WHERE celllinename = cl.celllinename) as fusiongeneexists,
   exists (SELECT rnaseqrunid FROM cellline.hla_a_type WHERE celllinename = cl.celllinename) as hlatypeexists,
   tumortype
 FROM cellline.cellline cl;
@@ -83,7 +74,7 @@ DROP MATERIALIZED VIEW IF EXISTS cellline.expressed_enst;
 
 CREATE MATERIALIZED VIEW cellline.expressed_enst AS
 SELECT p.enst, species
-  FROM cellline.processedrnaseqtranscript p 
+  FROM cellline.processedrnaseqtranscript p
     JOIN public.transcript t ON (p.enst = t.enst)
     JOIN public.gene g ON (g.ensg = t.ensg)
   GROUP BY p.enst, species
@@ -134,24 +125,13 @@ SELECT celllinename,
 
 ---
 
-DROP MATERIALIZED VIEW IF EXISTS cellline.hallmarkscore;
-
-CREATE MATERIALIZED VIEW cellline.hallmarkscore AS
-WITH hallmark AS (SELECT gene_set, species, unnest(ensg_array) AS ensg FROM msigdb WHERE gene_set LIKE 'HALLMARK%'),
-     expr AS (SELECT celllinename, ensg, log2tpm FROM cellline.processedrnaseqview WHERE ensg IN (SELECT distinct ensg FROM hallmark)),
-     stats AS (SELECT ensg, avg(log2tpm) AS log2tpm_mean, stddev(log2tpm) AS log2tpm_stddev FROM expr GROUP BY ensg HAVING stddev(log2tpm) > 0)
-SELECT gene_set, celllinename, sum(log2tpm) AS log2tpm_sum, sum((log2tpm - log2tpm_mean)/log2tpm_stddev) AS z_score_sum
-  FROM expr e INNER JOIN hallmark h ON e.ensg = h.ensg INNER JOIN stats s ON e.ensg = s.ensg GROUP BY gene_set, celllinename;
-
----
-
 DROP MATERIALIZED VIEW IF EXISTS cellline.mostvaryinggeneexpr;
 
 CREATE MATERIALIZED VIEW cellline.mostvaryinggeneexpr AS
 WITH rsspecies AS (SELECT DISTINCT species FROM cellline.rnaseqrun rsr JOIN cellline.cellline cl ON rsr.celllinename = cl.celllinename WHERE publish),
-     varying_ensg AS (SELECT ensg, species, variance, rank_number FROM rsspecies rss 
-                        JOIN LATERAL (SELECT ensg, variance(log2tpm) AS variance, rank() over(ORDER BY variance(log2tpm) DESC) rank_number FROM cellline.processedrnaseq prs JOIN cellline.rnaseqrun rsr ON prs.rnaseqrunid = rsr.rnaseqrunid 
-                                      JOIN cellline.cellline cl ON cl.celllinename = rsr.celllinename 
+     varying_ensg AS (SELECT ensg, species, variance, rank_number FROM rsspecies rss
+                        JOIN LATERAL (SELECT ensg, variance(log2tpm) AS variance, rank() over(ORDER BY variance(log2tpm) DESC) rank_number FROM cellline.processedrnaseq prs JOIN cellline.rnaseqrun rsr ON prs.rnaseqrunid = rsr.rnaseqrunid
+                                      JOIN cellline.cellline cl ON cl.celllinename = rsr.celllinename
                                       WHERE cl.species = rss.species AND publish GROUP BY ensg ORDER BY variance(log2tpm) DESC LIMIT 1000) AS t ON TRUE)
 SELECT pr.rnaseqrunid, pr.ensg, log2tpm, species, variance, rank_number FROM cellline.processedrnaseq pr JOIN cellline.rnaseqrun rr ON rr.rnaseqrunid = pr.rnaseqrunid JOIN varying_ensg ve ON (pr.ensg = ve.ensg) WHERE rr.publish;
 
@@ -161,9 +141,9 @@ SELECT pr.rnaseqrunid, pr.ensg, log2tpm, species, variance, rank_number FROM cel
 
 DROP VIEW IF EXISTS cellline.microsatelliteinstabilityview;
 CREATE VIEW cellline.microsatelliteinstabilityview AS
-SELECT dnaseqrunid, microsatellite_stability_score, microsatellite_stability_class, celllinename FROM 
+SELECT dnaseqrunid, microsatellite_stability_score, microsatellite_stability_class, celllinename FROM
 (
-  SELECT ms.dnaseqrunid, microsatellite_stability_score, microsatellite_stability_class, celllinename, ROW_NUMBER() OVER (PARTITION BY celllinename ORDER BY total_number_of_sites DESC) AS row_id 
+  SELECT ms.dnaseqrunid, microsatellite_stability_score, microsatellite_stability_class, celllinename, ROW_NUMBER() OVER (PARTITION BY celllinename ORDER BY total_number_of_sites DESC) AS row_id
   FROM cellline.microsatelliteinstability ms JOIN cellline.dnaseqrun r ON r.dnaseqrunid = ms.dnaseqrunid WHERE publish
 ) AS a WHERE Row_ID = 1;
 
@@ -172,60 +152,11 @@ SELECT dnaseqrunid, microsatellite_stability_score, microsatellite_stability_cla
 DROP VIEW IF EXISTS cellline.sequencingResults ;
 DROP VIEW IF EXISTS cellline.sequencingResultsSNP ;
 DROP VIEW IF EXISTS cellline.canonicalMutation;
-DROP VIEW IF EXISTS cellline.analysis;
 
 -------------------------------------------------
-
-CREATE VIEW cellline.analysis AS
-SELECT DISTINCT ensg, dnaseqrunid FROM cellline.analyzedexon ae INNER JOIN gene2transcript2exon g2t2e ON g2t2e.ense = ae.ense;
-
--------------------------------------------------
-
-CREATE VIEW cellline.canonicalMutation AS
-SELECT m.*, t.ENSG, t.transcriptname FROM cellline.mutation m, transcript t WHERE t.enst = m.enst AND t.iscanonical;
-
--------------------------------------------------
-CREATE VIEW cellline.sequencingResults AS
-SELECT ar.celllinename, ar.cellbatchid, c.cosmicid, ar.analysissource, g.symbol, g.name, ar.dnaseqrunid, 
-cm.chromosome, cm.startpos, cm.enst,
-cm.mutationeffect, cm.assembly, cm.genomicregion, cm.exon,
-cm.dnacoverage, rnacoverage, gnomAD_allelicFreq, oneKG_allelicFreq, 
-cm.cdnamutation, cm.proteinmutation, cm.snp, cm.snpsource, cm.dbsnpid,
-cm.dnazygosity, cm.qualityscore,
-cm.siftscore, g.ensg, g.biotype
-FROM cellline.dnaseqrun ar
-JOIN cellline.analysis a ON ar.dnaseqrunid = a.dnaseqrunid
-JOIN gene g ON (a.ENSG = g.ENSG)
-JOIN cellline.cellline c ON (c.celllinename = ar.celllinename)
-LEFT OUTER JOIN cellline.canonicalMutation cm ON (cm.dnaseqrunid = a.dnaseqrunid AND cm.ensg = a.ensg AND NOT snp)
-WHERE publish;
-
----------------------------------------------------
-
-CREATE VIEW cellline.sequencingResultsSNP AS
-SELECT ar.celllinename, ar.cellbatchid, c.cosmicid, ar.analysissource, g.symbol, g.name, ar.dnaseqrunid,
-cm.chromosome, cm.startpos, cm.enst,
-cm.mutationeffect, cm.assembly, cm.genomicregion, cm.exon,
-cm.dnacoverage, rnacoverage, gnomAD_allelicFreq, oneKG_allelicFreq,
-cm.cdnamutation, cm.proteinmutation, cm.snp, cm.snpsource, cm.dbsnpid,
-cm.dnazygosity, cm.qualityscore,
-cm.siftscore, g.ensg, g.biotype
-FROM cellline.dnaseqrun ar
-JOIN cellline.analysis a ON ar.dnaseqrunid = a.dnaseqrunid
-JOIN gene g ON (a.ENSG = g.ENSG)
-JOIN cellline.cellline c ON (c.celllinename = ar.celllinename)
-LEFT OUTER JOIN cellline.canonicalMutation cm ON (cm.dnaseqrunid = a.dnaseqrunid AND cm.ensg = a.ensg)
-WHERE publish;
-
----------------------------------------------------
 
 DROP VIEW IF EXISTS cellline.sequencingResultsAllTranscripts;
 DROP VIEW IF EXISTS cellline.allTranscriptMutation;
-
----------------------------------------------------
-
-CREATE VIEW cellline.allTranscriptMutation AS
-SELECT m.*, t.ENSG, t.transcriptname FROM cellline.mutation m, transcript t WHERE t.enst = m.enst;
 
 ---------------------------------------------------
 
@@ -244,10 +175,9 @@ SELECT m.*, t.ENSG, t.transcriptname FROM cellline.mutation m, transcript t WHER
 --WHERE publish;
 
 ----------------------------------------------------
-DROP VIEW IF EXISTS cellline.celllineMutCN;
-DROP VIEW IF EXISTS cellline.processedsequenceview;
+DROP VIEW IF EXISTS cellline.processedsequenceview CASCADE;
 
-CREATE VIEW cellline.processedsequenceview AS 
+CREATE VIEW cellline.processedsequenceview AS
 SELECT symbol, t.ensg, ps.* from cellline.processedsequence ps JOIN transcript t on (t.enst = ps.enst AND iscanonical) join gene g on (g.ensg = t.ensg);
 
 DROP VIEW IF EXISTS cellline.processedcopynumberview;
@@ -255,34 +185,25 @@ CREATE VIEW cellline.processedcopynumberview AS
 SELECT symbol, pcn.* FROM cellline.processedcopynumber pcn JOIN gene g ON (g.ensg = pcn.ensg);
 
 ----------------------------------------------------
-CREATE VIEW cellline.celllineMutCN AS
-SELECT gcl.ensg, gcl.symbol, gcl.celllinename, psv.enst, psv.dnamutation, psv.aamutation, psv.dnazygosity, psv.exonscomplete, psv.confirmeddetail, psv.numsources as processedsequencenumsources,
-       pcn.log2relativecopynumber, pcn.log2relativeCopyNumberDev, pcn.copynumbergainintron, pcn.copynumberlossintron, pcn.copynumbergainexon, pcn.copynumberlossexon,
-       pcn.gap, pcn.jump, pcn.exonicchange, pcn.intronicchange, pcn.numsources as processedcopynumbernumsources,
-       pcn.totalAbsCopyNumber, pcn.totalAbsCopyNumberDev, pcn.minorAbsCopyNumber, pcn.minorAbsCopyNumberDev, pcn.lossofheterozygosity 
-       FROM (SELECT ensg, symbol, celllinename FROM gene g, cellline.cellline cl) gcl 
-       LEFT JOIN cellline.processedsequenceview psv on (gcl.ensg = psv.ensg AND gcl.celllinename = psv.celllinename) 
-       LEFT JOIN cellline.processedcopynumber pcn on (gcl.ensg = pcn.ensg AND gcl.celllinename = pcn.celllinename);
-
 DROP VIEW IF EXISTS cellline.processedfusiongeneview;
 CREATE VIEW cellline.processedfusiongeneview AS
-SELECT pfg.processedfusion, pfg.celllinename, pfg.ensg1, pfg.ensg2, g1.symbol as symbol1, g2.symbol as symbol2, pfg.countsofcommonmappingreads, 
-       pfg.spanningpairs, pfg.spanninguniquereads, pfg.longestanchorfound, pfg.fusionfindingmethod, pfg.chrgene1, pfg.chrgene2, 
+SELECT pfg.processedfusion, pfg.celllinename, pfg.ensg1, pfg.ensg2, g1.symbol as symbol1, g2.symbol as symbol2, pfg.countsofcommonmappingreads,
+       pfg.spanningpairs, pfg.spanninguniquereads, pfg.longestanchorfound, pfg.fusionfindingmethod, pfg.chrgene1, pfg.chrgene2,
        pfg.nuclgene1, pfg.nuclgene2, pfg.strandgene1, pfg.strandgene2, pfg.rnaseqrunid, pfg.predictedeffect FROM cellline.processedfusiongene pfg
        JOIN gene g1 on (pfg.ensg1 = g1.ensg)
        JOIN gene g2 on (pfg.ensg2 = g2.ensg);
 
 DROP VIEW IF EXISTS cellline.processedrnaseqview CASCADE;
 CREATE VIEW cellline.processedrnaseqview AS
-SELECT prs.rnaseqrunid, celllinename, prs.ensg, prs.log2fpkm, prs.log2tpm, prs.log2cpm, prs.counts FROM cellline.processedrnaseq prs 
-  JOIN cellline.rnaseqrun nr ON nr.rnaseqrunid = prs.rnaseqrunid 
-  JOIN cellline.rnaseqgroup ng ON nr.rnaseqgroupid = ng.rnaseqgroupid 
+SELECT prs.rnaseqrunid, celllinename, prs.ensg, prs.log2fpkm, prs.log2tpm, prs.log2cpm, prs.counts FROM cellline.processedrnaseq prs
+  JOIN cellline.rnaseqrun nr ON nr.rnaseqrunid = prs.rnaseqrunid
+  JOIN cellline.rnaseqgroup ng ON nr.rnaseqgroupid = ng.rnaseqgroupid
   WHERE nr.canonical AND (rnaseqname like 'untreated %cellline reference set%' AND processingpipeline LIKE 'RNA-seq%');
 
 --DROP VIEW IF EXISTS cellline.processedrnaseqview2 CASCADE;
 --CREATE VIEW cellline.processedrnaseqview2 AS
 --WITH rnaseqrun2 AS (
---  SELECT  nr.rnaseqrunid, nr.celllinename 
+--  SELECT  nr.rnaseqrunid, nr.celllinename
 --  FROM cellline.rnaseqrun nr
 --  JOIN cellline.rnaseqgroup ng ON nr.rnaseqgroupid = ng.rnaseqgroupid
 --  WHERE nr.canonical AND (rnaseqname like 'untreated %cellline reference set%' AND processingpipeline LIKE 'RNA-seq%')
@@ -292,13 +213,26 @@ SELECT prs.rnaseqrunid, celllinename, prs.ensg, prs.log2fpkm, prs.log2tpm, prs.l
 
 DROP VIEW IF EXISTS cellline.processedrnaseqtranscriptview CASCADE;
 CREATE VIEW cellline.processedrnaseqtranscriptview AS
-SELECT prs.rnaseqrunid, celllinename, prs.enst, prs.log2fpkm, prs.log2tpm, prs.counts FROM cellline.processedrnaseqtranscript prs 
-  JOIN cellline.rnaseqrun nr ON nr.rnaseqrunid = prs.rnaseqrunid    
-  JOIN cellline.rnaseqgroup ng ON nr.rnaseqgroupid = ng.rnaseqgroupid 
+SELECT prs.rnaseqrunid, celllinename, prs.enst, prs.log2fpkm, prs.log2tpm, prs.counts FROM cellline.processedrnaseqtranscript prs
+  JOIN cellline.rnaseqrun nr ON nr.rnaseqrunid = prs.rnaseqrunid
+  JOIN cellline.rnaseqgroup ng ON nr.rnaseqgroupid = ng.rnaseqgroupid
   WHERE nr.canonical AND (rnaseqname like 'untreated %cellline reference set%' AND processingpipeline LIKE 'RNA-seq%');
 
 --CREATE VIEW cellline.processedrnaseqview AS
 --SELECT prs.rnaseqrunid, celllinename, prs.ensg, prs.log2fpkm, prs.log2tpm, prs.counts FROM cellline.rnaseqrun nr JOIN cellline.processedrnaseq prs ON (nr.rnaseqrunid = prs.rnaseqrunid) WHERE nr.canonical;
+
+---
+
+DROP MATERIALIZED VIEW IF EXISTS cellline.hallmarkscore;
+
+CREATE MATERIALIZED VIEW cellline.hallmarkscore AS
+WITH hallmark AS (SELECT gene_set, species, unnest(ensg_array) AS ensg FROM msigdb WHERE gene_set LIKE 'HALLMARK%'),
+     expr AS (SELECT celllinename, ensg, log2tpm FROM cellline.processedrnaseqview WHERE ensg IN (SELECT distinct ensg FROM hallmark)),
+     stats AS (SELECT ensg, avg(log2tpm) AS log2tpm_mean, stddev(log2tpm) AS log2tpm_stddev FROM expr GROUP BY ensg HAVING stddev(log2tpm) > 0)
+SELECT gene_set, celllinename, sum(log2tpm) AS log2tpm_sum, sum((log2tpm - log2tpm_mean)/log2tpm_stddev) AS z_score_sum
+  FROM expr e INNER JOIN hallmark h ON e.ensg = h.ensg INNER JOIN stats s ON e.ensg = s.ensg GROUP BY gene_set, celllinename;
+
+---
 
 DROP VIEW IF EXISTS cellline.processeddepletionscoreview CASCADE;
 CREATE VIEW cellline.processeddepletionscoreview AS
@@ -307,7 +241,6 @@ JOIN gene g ON (d.ENSG = g.ENSG);
 
 DROP MATERIALIZED VIEW IF EXISTS cellline.mutationalburden CASCADE;
 CREATE MATERIALIZED VIEW cellline.mutationalburden AS
-SELECT psv.celllinename, species, tumortype, sum((aamutation <> 'wt')::INT4)/count(*)::REAL AS mutational_fraction 
-  FROM cellline.processedsequenceview psv JOIN cellline.cellline cl ON cl.celllinename = psv.celllinename 
+SELECT psv.celllinename, species, tumortype, sum((aamutation <> 'wt')::INT4)/count(*)::REAL AS mutational_fraction
+  FROM cellline.processedsequenceview psv JOIN cellline.cellline cl ON cl.celllinename = psv.celllinename
   GROUP BY psv.celllinename, species, tumortype having count(*) > 10000;
-
