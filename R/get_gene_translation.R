d3031097010e2d828da4446d8f0672eb42e8233d
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-15 08:06:47 +0100

    added library to function call

--- R/get_gene_translation.R
+++ R/get_gene_translation.R
@@ -1,14 +1,14 @@
 get_gene_translation <- function(geneid) {
 
   geneid_unique <- unique(geneid)
-  
+
   con <- getPostgresqlConnection()
-  
+
   # ------ translate including same gene symbol
   # sql <- paste0("SELECT g.ensg, g2e.geneid, g.symbol as ensg_symbol, e.symbol as gene_symbol FROM gene g ",
   #               "JOIN normchromentrezgene2ensemblgene g2e on g.ensg = g2e.ensg ",
   #               "JOIN entrezgene e ON e.geneid = g2e.geneid ",
-  #               "WHERE g.symbol = e.symbol AND e.symbol IS NOT NULL AND g2e.geneid in (", 
+  #               "WHERE g.symbol = e.symbol AND e.symbol IS NOT NULL AND g2e.geneid in (",
   #               paste(geneid_unique, collapse = ","), ")")
 
   # ----- translate only based on gene to ensembl link
@@ -16,16 +16,16 @@ get_gene_translation <- function(geneid) {
                 "JOIN normchromentrezgene2ensemblgene g2e on g.ensg = g2e.ensg ",
                 "JOIN entrezgene e ON e.geneid = g2e.geneid ",
                 "WHERE g2e.geneid in (", paste(geneid_unique, collapse = ","), ")")
-  
+
   ensg <- RPostgres::dbGetQuery(con, sql)
   RPostgres::dbDisconnect(con)
-  
+
   ensg_summary <- ensg %>%
-    group_by(geneid) %>% 
-    summarize(n = n()) %>% 
-    filter(n > 1) # assign all ambiguous geneid <=> ensg associations
-  
-  ensg %>% 
-    filter(!geneid %in% ensg_summary$geneid) %>% # filter ambiguous gene annotations
-    select(ensg, geneid)
+    dplyr::group_by(geneid) %>%
+    dplyr::summarize(n = n()) %>%
+    dplyr::filter(n > 1) # assign all ambiguous geneid <=> ensg associations
+
+  ensg %>%
+    dplyr::filter(!geneid %in% ensg_summary$geneid) %>% # filter ambiguous gene annotations
+    dplyr::select(ensg, geneid)
 }
