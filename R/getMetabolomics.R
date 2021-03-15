d44f69ae61d1b05ab3abcd19dccfd9640d29eae3
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-15 08:02:26 +0100

    formatting

--- R/getMetabolomics.R
+++ R/getMetabolomics.R
@@ -1,27 +1,37 @@
 getMetabolomics <- function() {
-  
+
   con <- getPostgresqlConnection()
-  
+
   cellline <- dplyr::tbl(con, dbplyr::in_schema("cellline", "cellline"))  %>%
     dplyr::filter(species == "human") %>%
     dplyr::select(celllinename, depmap) %>%
     dplyr::collect()
-  
+
   RPostgres::dbDisconnect(con)
-  
+
   # ---------------
-  
-  CCLE.metabolomics <- getFileData("CCLE_metabolomics_20190502") %>%
-    as.data.frame() %>%
-    tibble::rownames_to_column("depmap") %>%
+
+  CCLE.metabolomics <- getFileData("CCLE_metabolomics_20190502")
+
+  if ("matrix" %in% class(CCLE.metabolomics)) {
+    CCLE.metabolomics <- CCLE.metabolomics %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column("depmap")
+  } else {
+    CCLE.metabolomics <- CCLE.metabolomics %>%
+      dplyr::rename(depmap = DepMap_ID) %>%
+      dplyr::select(-CCLE_ID)
+  }
+
+  CCLE.metabolomics <- CCLE.metabolomics %>%
     tidyr::pivot_longer(!depmap, names_to = "metabolite", values_to = "score") %>%
     dplyr::inner_join(cellline, by = "depmap") %>%
     dplyr::select(-depmap) %>%
     dplyr::distinct(celllinename, metabolite, .keep_all = TRUE)
-  
+
   metabolites <- CCLE.metabolomics %>% select(metabolite) %>% unique()
-  
+
   list(cellline.metabolite = metabolites,
        cellline.processedmetabolite = CCLE.metabolomics)
-  
+
 }
