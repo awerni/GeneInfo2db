bb67ada03961ede93fa403c49bac8d967f152f95
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-15 08:03:31 +0100

    write to other table, added lab, added campaign

--- R/getPrism.R
+++ R/getPrism.R
@@ -1,25 +1,27 @@
 getPrism <- function() {
-  
+
   con <- getPostgresqlConnection()
-  
+
   cellline <- dplyr::tbl(con, dbplyr::in_schema("cellline", "cellline"))  %>%
     dplyr::filter(species == "human") %>%
     dplyr::select(celllinename, depmap) %>%
     dplyr::collect()
-  
+
   RPostgres::dbDisconnect(con)
-  
+
   # -----------------------------------
-  
-  prism_data <- getTaiga("secondary-dose-response-curve-parameters")
-  
-  prism_drugs <- prism_data %>% 
-    dplyr::select(name, moa, target, indication, smiles) %>% 
+
+  dfile <- "secondary-dose-response-curve-parameters"
+  if (getOption("useFileDownload")) dfile <- "secondary-screen-dose-response-curve-parameters"
+  prism_data <- getFileData(dfile)
+
+  prism_drugs <- prism_data %>%
+    dplyr::select(name, moa, target, indication, smiles) %>%
     unique() %>%
     dplyr::mutate(scientificname = name, commonname = NA) %>%
     dplyr::rename(drugid = name) %>%
     dplyr::filter(!is.na(drugid))
-  
+
   prism_data2 <- prism_data %>%
     dplyr::select(-moa, -target, -disease.area, -indication, -phase, -smiles, -row_name, -ccle_name) %>%
     dplyr::filter(passed_str_profiling, !is.na(name)) %>%
@@ -28,12 +30,15 @@ getPrism <- function() {
     dplyr::slice(1) %>%
     dplyr::ungroup() %>%
     dplyr::inner_join(cellline, by = c("depmap_id" = "depmap")) %>%
-    dplyr::mutate(campaign = 'Prism', proliferationtest = "PRISM") %>%
+    dplyr::mutate(campaign = 'Prism', proliferationtest = "PRISM", laboratory = "Broad Institute") %>%
     dplyr::rename(actarea = auc, drugid = name, top = upper_limit, bottom = lower_limit) %>%
     dplyr::mutate(actarea = 1 - actarea) %>%
     dplyr::select(-broad_id, -depmap_id, -screen_id, -passed_str_profiling, -r2) %>%
     dplyr::mutate(ec50 = ifelse(ec50 > 1e5 | ec50 < 1e-5, NA, ec50), ic50 = ifelse(ic50 > 1e5 | ic50 < 1e-5, NA, ic50))
-  
+
+  campaign <- data.frame(campaign = "Prism", campaigndesc = "profiling relative inhibition simultaneously in mixtures")
+
   list(public.drug = prism_drugs,
-       cellline.doseresponsecurve = prism_data2)
+       cellline.campaign = campaign,
+       cellline.processedproliftest = prism_data2)
 }
