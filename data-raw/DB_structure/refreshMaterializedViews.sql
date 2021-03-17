4cab9807f36dc79c7650ad6e503e09ba82f109cb
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-17 09:06:41 +0100

    inital import

---
+++ data-raw/DB_structure/refreshMaterializedViews.sql
@@ -0,0 +1,11 @@
+refresh materialized view cellline.cnaltered_ensg;
+refresh materialized view cellline.expressed_ensg;
+refresh materialized view cellline.expressed_enst;
+refresh materialized view cellline.hallmarkscore;
+refresh materialized view cellline.mostvaryinggeneexpr;
+refresh materialized view cellline.mutationalburden;
+refresh materialized view cellline.processedcopynumber_array;
+refresh materialized view cellline.processedrnaseq_array;
+refresh materialized view cellline.processedrnaseqtranscript_array;
+refresh materialized view cellline.sequenced_transcript;
+
