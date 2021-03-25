7c95a17b52843372e11a2202d5187f004be00800
Andreas Wernitznig <andreas@wernitznig.com>
2021-03-25 20:20:45 +0100

    removed alt cell line names bug

--- data-raw/db_structure.R
+++ data-raw/db_structure.R
@@ -22,7 +22,7 @@ db_glue <- paste(db_glue_list, collapse = "")
 refreshView <- readr::read_file(paste0(p, "refreshMaterializedViews.sql")) %>%
   gsub(pattern = "(\n|\r)", replacement = "")
 
-alternative_celllinename <- readr::read_file(paste0(p, "refreshMaterializedViews.sql")) %>%
+alternative_celllinename <- readr::read_file(paste0(p, "alternative_celllinenames.sql")) %>%
   gsub(pattern = "(\n|\r)", replacement = "")
 
 save(recreateSchema, geneAnnotation, celllineDB, db_glue, refreshView, alternative_celllinename, file = "data/db_schema.rdata")
