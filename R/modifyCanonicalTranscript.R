0e07908026a747d58cbfe2b498dd341f070af0d4
wernitzn <andreas.wernitznig@boehringer-ingelheim.com>
2021-03-27 18:49:18 +0100

    modify canonical transcript to sequenced one

--- R/modifyCanonicalTranscript.R
+++ R/modifyCanonicalTranscript.R
@@ -1,19 +1,24 @@
 modifyCanonicalTranscript <- function() {
   con <- getPostgresqlConnection()
   
-  sql <- paste0("SELECT t ensg, enst, iscanonical FROM transcript ",
+  sql <- paste0("SELECT ensg, enst, iscanonical FROM transcript ",
                 "WHERE enst IN (SELECT distinct enst FROM cellline.processedsequence)")
   
   mut_enst <- DBI::dbGetQuery(con, sql)
   
   mut_enst2 <- mut_enst %>% group_by(ensg, iscanonical) %>%
-    summarise(n = n()) %>%
+    summarise(n = n(), .groups = "drop") %>%
     pivot_wider(ensg, names_from = "iscanonical", values_from = "n", values_fill = 0) %>%
     rename(canonical = 2, non_canonical = 3) %>%
-    filter(canonical == 0 & non_canonical == 1) %>%
-    mutate(sql = paste0("UPDATE transcript SET iscanonical = TRUE WHERE enst = '", enst, "'"))
+    filter(canonical == 0 & non_canonical == 1)
   
-  sapply(mut_enst2$sql, function(s) DBI::dbGetQuery(con, s))
+  mut_enst3 <- mut_enst %>%
+    filter(ensg %in% mut_enst2$ensg) %>%
+    mutate(sql1 = paste0("UPDATE transcript SET iscanonical = FALSE WHERE ensg = '", ensg, "'"),
+           sql2 = paste0("UPDATE transcript SET iscanonical = TRUE WHERE enst = '", enst, "'"))
+  
+  sapply(mut_enst3$sql1, function(s) DBI::dbExecute(con, s))
+  sapply(mut_enst3$sql2, function(s) DBI::dbExecute(con, s))
   
   RPostgres::dbDisconnect(con)
   
