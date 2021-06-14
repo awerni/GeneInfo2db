46a27393fe07e9c8efa2ddad6146c0c88889d427
Zygmunt Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-06-14 10:29:04 +0200

    Utuility functions for managing local file cache. Required by safeRead function.

--- R/loadSettings.R
+++ R/loadSettings.R
@@ -2,5 +2,6 @@ loadSettings <- function() {
   options("dbname" = "bioinfo.hg38")
   options("dbhost" = "vie-bio-postgres")
   options("useFileDownload" = TRUE)
-  options("msigdb_path" = "~/Download/")
+  options("msigdb_path" = "~/Downloads/")
+  options("geneinfo2db_local_filecache" = "~/Downloads/geneinfo2db_local_filecache")
 }
---
+++ R/localFileCache.R
@@ -0,0 +1,67 @@
+#' Check if geneinfo2db_local_filecache direcory exists. If not creates this path.
+#'
+#' @param subdir direcotry for storing local copies of remote files.
+#'
+#' @return path 
+#'
+#' @examples
+#' 
+#' \donttest{
+#' checkLocalFileRepo(tempdir())
+#' }
+#' 
+checkLocalFileRepo <- function(subdir = getOption("geneinfo2db_local_filecache")) {
+  log_trace("Check for subdir '{subdir}' existence: {dir.exists(subdir)}")
+  if(!dir.exists(subdir)) {
+    log_trace("'{subdir}' does not exists. Creating it.")
+    dir.create(subdir, showWarnings = FALSE, recursive = TRUE)
+  }
+  return(subdir)
+}
+
+#' Returns the path for local file cache.
+#'
+#' @return Path to local file cache.
+#'
+#' @details This function also checks if the local cache exists by calling \code{\link{checkLocalFileRepo}}.
+#'
+#' @examples
+#' 
+#' \dontrun{
+#' getLocalFileRepo()
+#' }
+#' 
+getLocalFileRepo <- function() {
+  path <- getOption("geneinfo2db_local_filecache", "")
+  checkLocalFileRepo(path)
+  path
+}
+
+#' Set path to local file cache.
+#'
+#' @param path path to local file cache.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+setLocalFileRepo <- function(path) {
+  options("geneinfo2db_local_filecache" = path)
+  invisible(getLocalFileRepo())
+}
+
+#' Modify file path to include local file cache.
+#'
+#' @param path path to file to download.
+#'
+#' @return a file path consisting the local file chace path.
+#' @examples
+#' 
+#' \dontrun{
+#' useLocalFileRepo("xd.rds")
+#' }
+#' 
+useLocalFileRepo <- function(path) {
+  file.path(getLocalFileRepo(), path)
+}
