6c86530ff383d75e0985561c26d82c6df8e047bc
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-09-24 12:40:28 +0200

    skip refseq

--- R/getEntrez.R
+++ R/getEntrez.R
@@ -1,6 +1,9 @@
 getEntrez <- function(gene_info, refseq_info, species_name) {
   res <- getEntrezGene(gene_info, species_name)
-  res$public.refseq <- getRefseq(refseq_info, species_name)
+  # The RefSeq is a legacy functionality to compare sequences.
+  # Currently we are not using RefSeq in CLIFF.
+  # It's nice to have but if it causes problems we simply skip it.
+  # res$public.refseq <- getRefseq(refseq_info, species_name)
   
   return(res)
 }
