660672b988f96337035167bbe9fc8bad33ca6947
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-09-27 11:02:55 +0200

    use sanger-ceres files

--- R/getSanger.R
+++ R/getSanger.R
@@ -2,10 +2,10 @@ getSanger <- function() {
   get_CRISPR_screen(
     screen_name = 'Sanger',
     screen_desc = 'Sanger CRISPR Screen',
-    file_essentials = "essential_genes@sanger",
-    file_nonessentials = "nonessential_genes@sanger",
-    file_effect = "gene_effect@sanger",
-    file_effect_unscaled = "gene_effect_unscaled@sanger",
-    file_dependency = "gene_dependency@sanger"
+    file_essentials = "essential_genes@sanger-ceres",
+    file_nonessentials = "nonessential_genes@sanger-ceres",
+    file_effect = "gene_effect@sanger-ceres",
+    file_effect_unscaled = "gene_effect_unscaled@sanger-ceres",
+    file_dependency = "gene_dependency@sanger-ceres"
   )
 }
