992453326901f5f8c26646b868c9be1b868e10e3
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-10-06 14:19:11 +0200

    Do not use default values for database connection, but allow user to specify them easily.

--- R/loadSettings.R
+++ R/loadSettings.R
@@ -1,4 +1,16 @@
-parsePgPass <- function(.pgpassPaths = c(".pgpass", "~/.pgpass")) {
+#' Get database config from .pgpass file
+#'
+#' @param line which line of .pgpass should be used
+#' @param .pgpassPaths path to .pgpass file. Default value should work in most 
+#' cases.
+#'
+#' @return list with database configuration.
+#' 
+#' @note To use specific config, it need to be set using \code{setDBconfig}.
+#' 
+#' @export
+#' 
+pgpass2dbConfig <- function(line = 1, .pgpassPaths = c(".pgpass", "~/.pgpass")) {
   
   existingFiles <- .pgpassPaths[file.exists(.pgpassPaths)]
   
@@ -11,29 +23,85 @@ parsePgPass <- function(.pgpassPaths = c(".pgpass", "~/.pgpass")) {
   file <- existingFiles[1]
   content <- readLines(file, warn = FALSE)
   
+  if(line > length(content)) {
+    stop(".pgpass does not have ", line, " lines")
+  }
+  content <- content[[line]]
+  
   pgpassContent <- strsplit(content, split = ":")[[1]]
   
-  options("GeneInfo2db.DB_HOST" = pgpassContent[1])
-  options("GeneInfo2db.DB_PORT" = as.integer(pgpassContent[2]))
-  options("GeneInfo2db.DB_NAME" = pgpassContent[3])
-  options("GeneInfo2db.DB_USER" = pgpassContent[4])
-  options("GeneInfo2db.DB_PASSWORD" = pgpassContent[5])
+  config <- list(
+    host = pgpassContent[1], 
+    port = as.integer(pgpassContent[2]),
+    name = pgpassContent[3],
+    user = pgpassContent[4],
+    pass = pgpassContent[5]
+  )
   
-  message("Default postgres config infered from ", file, ":")
+  class(config) <- "GeneInfo2DatabaseConfig"
+  config
+}
+
+#' Method for printing GeneInfo2DatabaseConfig object.
+#'
+#' @param x GeneInfo2DatabaseConfig object.
+#' @param ... not used.
+#'
+#' @details this function hides the password value.
+#'
+#' @export print.GeneInfo2DatabaseConfig
+#'
+print.GeneInfo2DatabaseConfig <- function(x, ...) {
+  
+  print(x[-5])
+  cat("$pass\n    <hidden>")
+  
+  invisible(x)
+}
+
+#' Set database config.
+#'
+#' @param host database host or list with configuration (preferably the result 
+#' of pgpass2dbConfig)
+#' @param port database port
+#' @param name database name
+#' @param user database user
+#' @param pass database password
+#'
+#' @note the order of arguments mimics the order values in .pgpass file.
+#'
+#' @return nothing. Called for side effects.
+#' @export
+#'
+setDBconfig <- function(host, port, name, user, pass) {
+  
+  if(is.list(host)) {
+    port <- host[["port"]]
+    name <- host[["name"]]
+    user <- host[["user"]]
+    pass <- host[["pass"]]
+    host <- host[["host"]]
+  }
+  
+  port <- as.integer(port)
+  
+  options("GeneInfo2db.DB_HOST" = host)
+  options("GeneInfo2db.DB_PORT" = port)
+  options("GeneInfo2db.DB_NAME" = name)
+  options("GeneInfo2db.DB_USER" = user)
+  options("GeneInfo2db.DB_PASSWORD" = pass)
+  
+  message("Data base config:")
   message("GeneInfo2db.DB_HOST: ", getOption("GeneInfo2db.DB_HOST"))
   message("GeneInfo2db.DB_PORT: ", getOption("GeneInfo2db.DB_PORT"))
   message("GeneInfo2db.DB_NAME: ", getOption("GeneInfo2db.DB_NAME"))
   message("GeneInfo2db.DB_USER: ", getOption("GeneInfo2db.DB_USER"))
-  message("GeneInfo2db.DB_PASSWORD: ", 'getOption("GeneInfo2db.DB_PASSWORD"))')
+  message("GeneInfo2db.DB_PASSWORD: ", 'getOption("GeneInfo2db.DB_PASSWORD")')
   
 }
 
 loadSettings <- function() {
-  
   registerGeneInfoDownloadFunction()
-  options("dbname" = "bioinfo.hg38")
-  options("dbhost" = "vie-bio-postgres")
   options("msigdb_path" = "~/Downloads/msigdb")
-  parsePgPass()
   options("geneinfo2db_local_filecache" = "~/Downloads/geneinfo2db_local_filecache")
 }
