900ce9233d895cc11932eed0d2e0f5f1ffc2adf1
Andreas Wernitznig <andreas@wernitznig.com>
2021-11-12 08:57:40 +0100

    alternatively use environment variable to find .pgpass

--- R/loadSettings.R
+++ R/loadSettings.R
@@ -2,51 +2,51 @@
 #'
 #' @param dbsettings incomplete list of dbhost, dbname, dbuser, dbport, or dbpass
 #' @return complete list with database configuration.
-#' 
+#'
 #' @note the return value of this function is used to specify DB config for \code{setDBconfig}.
-#' 
+#'
 #' @export
-#' 
+#'
 getDBConfig <- function(dbsettings) {
-  
+
   pgpassFile <- Sys.getenv("PGPASSFILE")
   if (pgpassFile == "") pgpassFile <- "~/.pgpass"
 
   if(!file.exists(pgpassFile)) {
-    message("No .pgpass file found. ")
+    message("No pgpass file found. ")
     return()
   }
-  
+
   fillMissing <- function(x, alt) {
     ifelse(is.null(x) || is.na(x), alt, x)
-  } 
-  
+  }
+
   DBname <- fillMissing(dbsettings[["dbname"]], as.character(NA))
   DBhost <- fillMissing(dbsettings[["dbhost"]], Sys.getenv("PGHOST"))
   DBport <- fillMissing(dbsettings[["dbport"]], "5432")
   DBuser <- fillMissing(dbsettings[["dbuser"]], Sys.getenv("USER"))
   DBpass <- fillMissing(dbsettings[["dbpass"]], as.character(NA))
-  
+
   if (DBname == "" | is.na(DBname) ) {
     message("No database name found.")
     return()
   }
-  
+
   if (DBhost == "" | is.na(DBhost)) {
     message("No database hostname found.")
     return()
   }
-  
+
   compare <- function(x, y) (x == y | y == "*")
-  
+
   if (DBpass == "" | is.na(DBpass) | is.null(DBpass)) {
-    pgpass <- strsplit(scan("~/.pgpass", what = "", quiet = TRUE), ":")
+    pgpass <- strsplit(scan(pgpassFile, what = "", quiet = TRUE), ":")
     n <- sapply(pgpass, function(x) {
       compare(DBhost, x[[1]]) & compare(DBport, x[[2]]) & compare(DBname, x[[3]]) & compare(DBuser, x[[4]])
     })
     DBpass <- pgpass[[which(n)[1]]][[5]]
   }
- 
+
   config <- list(
     host = DBhost,
     port = DBport,
@@ -54,7 +54,7 @@ getDBConfig <- function(dbsettings) {
     user = DBuser,
     pass = DBpass
   )
-  
+
   class(config) <- "GeneInfo2DatabaseConfig"
   config
 }
@@ -69,16 +69,16 @@ getDBConfig <- function(dbsettings) {
 #' @export print.GeneInfo2DatabaseConfig
 #'
 print.GeneInfo2DatabaseConfig <- function(x, ...) {
-  
+
   print(x[-5])
   cat("$pass\n    <hidden>\n")
-  
+
   invisible(x)
 }
 
 #' Set database config.
 #'
-#' @param host database host or list with configuration (preferably the result 
+#' @param host database host or list with configuration (preferably the result
 #' of pgpass2dbConfig)
 #' @param port database port
 #' @param name database name
@@ -91,7 +91,7 @@ print.GeneInfo2DatabaseConfig <- function(x, ...) {
 #' @export
 #'
 setDBconfig <- function(host, port, name, user, pass) {
-  
+
   if(is.list(host)) {
     port <- host[["port"]]
     name <- host[["name"]]
@@ -99,22 +99,22 @@ setDBconfig <- function(host, port, name, user, pass) {
     pass <- host[["pass"]]
     host <- host[["host"]]
   }
-  
+
   port <- as.integer(port)
-  
+
   options("GeneInfo2db.DB_HOST" = host)
   options("GeneInfo2db.DB_PORT" = port)
   options("GeneInfo2db.DB_NAME" = name)
   options("GeneInfo2db.DB_USER" = user)
   options("GeneInfo2db.DB_PASSWORD" = pass)
-  
+
   message("Database config:")
   message("GeneInfo2db.DB_HOST: ", getOption("GeneInfo2db.DB_HOST"))
   message("GeneInfo2db.DB_PORT: ", getOption("GeneInfo2db.DB_PORT"))
   message("GeneInfo2db.DB_NAME: ", getOption("GeneInfo2db.DB_NAME"))
   message("GeneInfo2db.DB_USER: ", getOption("GeneInfo2db.DB_USER"))
   message("GeneInfo2db.DB_PASSWORD: ", 'getOption("GeneInfo2db.DB_PASSWORD")')
-  
+
 }
 
 loadSettings <- function() {
