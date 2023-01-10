68a6f754468e8e83a52d880fa52cff1245a3c2ab
Andreas Wernitznig <andreas@wernitznig.com>
2023-01-10 09:34:45 +0100

    removed cellline.hallmarkscore

--- data-raw/DB_structure/refreshMaterializedViewsCellline.sql
+++ data-raw/DB_structure/refreshMaterializedViewsCellline.sql
@@ -1,7 +1,6 @@
 refresh materialized view cellline.cnaltered_ensg;
 refresh materialized view cellline.expressed_ensg;
 refresh materialized view cellline.expressed_enst;
-refresh materialized view cellline.hallmarkscore;
 refresh materialized view cellline.mostvaryinggeneexpr;
 refresh materialized view cellline.mutationalburden;
 refresh materialized view cellline.processedcopynumber_array;
