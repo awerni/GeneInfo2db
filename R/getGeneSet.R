f02f7d32b9e543c05955fb08cb2f107ac1c6ade9
Andreas Wernitznig <andreas@wernitznig.com>
2023-01-10 09:30:01 +0100

    added non-essentials as additional gene set

--- R/getGeneSet.R
+++ R/getGeneSet.R
@@ -1,18 +1,27 @@
 getGeneSet <- function() {
-  essential_genes <- getFileData("common_essentials")
-  gene_df <- separate_gene(essential_genes)
-  
+  essential_genes <- getFileData("AchillesCommonEssentialControls") %>%
+    dplyr::rename(gene = Gene) %>%
+    dplyr::mutate(genesetname = "essential genes")
+
+  nonessential_genes <- getFileData("AchillesNonessentialControls") %>%
+    dplyr::rename(gene = Gene) %>%
+    dplyr::mutate(genesetname = "non-essential genes")
+
+  gene_df <- separate_gene(dplyr::bind_rows(essential_genes, nonessential_genes)) %>%
+    dplyr::select(-symbol)
+
   geneset <- tibble::tribble(
     ~genesetname, ~species,
-    "essential genes", "human"
+    "essential genes", "human",
+    "non-essential genes", "human"
   )
-  
+
   geneassignment <- get_gene_translation(gene_df$geneid) %>%
-    dplyr::select(-geneid) %>%
-    dplyr::mutate(genesetname = "essential genes")
-  
+    dplyr::left_join(gene_df, by = "geneid") %>%
+    dplyr::select(-geneid)
+
   list(
     public.geneset = geneset,
-    public.geneassignment = geneassignment   
+    public.geneassignment = geneassignment
   )
 }
