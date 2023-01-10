b413d1103533ed26ea0e3e947a2f3211b1c8aa48
Andreas Wernitznig <andreas@wernitznig.com>
2023-01-10 13:07:58 +0100

    added library name to functions

--- R/createCelllineSigRAS.R
+++ R/createCelllineSigRAS.R
@@ -1,42 +1,42 @@
 # https://doi.org/10.1016/j.cell.2018.03.035
 createCelllineSigRAS <- function() {
-  
+
   con <- getPostgresqlConnection()
-  
+
   cellline <- dplyr::tbl(con, dbplyr::in_schema("cellline", "cellline"))  %>%
     dplyr::filter(species == "human") %>%
     dplyr::select(celllinename) %>%
     dplyr::collect()
-  
+
   alternative_celllinename <- dplyr::tbl(con, dbplyr::in_schema("cellline", "alternative_celllinename"))  %>%
     dplyr::select(celllinename, alternative_celllinename) %>%
     dplyr::collect()
-  
+
   RPostgres::dbDisconnect(con)
-  
+
   file <- "https://raw.githubusercontent.com/greenelab/pancancer/master/results/ccle_Ras_classifier_scores.tsv"
   data <- read_tsv(file) %>%
     select(l = cell_line, score = weight) %>%
-    separate(col = l, c("celllinename", "cl2"), extra ="drop") %>% 
-    select(-cl2) 
-  
+    separate(col = l, c("celllinename", "cl2"), extra ="drop") %>%
+    select(-cl2)
+
   unknown <- setdiff(data$celllinename, cellline$celllinename)
   if (length(unknown) > 0) {
     data <- data %>%
-      left_join(alternative_celllinename, by = c("celllinename" = "alternative_celllinename")) %>%
-      mutate(celllinename = ifelse(is.na(celllinename.y), celllinename, celllinename.y)) %>%
-      filter(celllinename %in% cellline$celllinename) %>%
-      select(-celllinename.y) %>%
+      dplyr::left_join(alternative_celllinename, by = c("celllinename" = "alternative_celllinename")) %>%
+      dplyr::mutate(celllinename = ifelse(is.na(celllinename.y), celllinename, celllinename.y)) %>%
+      dplyr::filter(celllinename %in% cellline$celllinename) %>%
+      dplyr::select(-celllinename.y) %>%
       unique()
   }
-  
+
   data2 <- data %>%
     mutate(signature = "RAS")
-  
+
   signature_db <- data.frame(
     signature   = "RAS",
-    description = "RAS activation signature", 
-    unit        = "arbitrary units", 
+    description = "RAS activation signature",
+    unit        = "arbitrary units",
     hyperlink   = "https://doi.org/10.1016/j.celrep.2018.03.046"
   )
 
