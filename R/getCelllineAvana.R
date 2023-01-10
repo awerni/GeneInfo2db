9b664163b7fb194c3edc30840fe5f4cdcc7f12b2
Andreas Wernitznig <andreas@wernitznig.com>
2023-01-10 09:30:40 +0100

    new file name in 22Q4

--- R/getCelllineAvana.R
+++ R/getCelllineAvana.R
@@ -8,23 +8,22 @@ getCelllineAvana <- function() {
   #   file_effect_unscaled = "Achilles_gene_effect_unscaled_CERES",
   #   file_dependency = "Achilles_gene_dependency_CERES"
   # )
-  
+
   chronos <- getCellline_CRISPR_screen_chronos(
     screen_name = 'Avana', 
     screen_desc = 'Broad Institute DepMap Avana CRISPR Screen',
-    file_effect = "Achilles_gene_effect",
-    file_dependency = "Achilles_gene_dependency"
+    file_effect = "CRISPRGeneEffect",
+    file_dependency = "CRISPRGeneDependency"
   )
-  
+
   # fullData <- full_join(
   #   ceres$cellline.processeddepletionscore,
   #   chronos$cellline.processeddepletionscore,
   #   by = c("celllinename", "ensg", "depletionscreen")
   # )
-  
+
   list(
     cellline.depletionscreen = chronos$cellline.depletionscreen,
     cellline.processeddepletionscore = chronos$cellline.processeddepletionscore
   )
-  
 }
