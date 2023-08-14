aa4786746b93199107ccdaea455256dc7b687f93
awerni <andreas@wernitznig.com>
2023-08-14 15:23:58 +0200

    inital import

---
+++ R/getMissingLab.R
@@ -0,0 +1,15 @@
+getMissingLab <- function(lab) {
+  con <- getPostgresqlConnection()
+
+  laboratory <- dplyr::tbl(con, "laboratory") |>
+    dplyr::collect()
+
+  RPostgres::dbDisconnect(con)
+
+  # -------------------------
+  if (lab %in% laboratory$laboratory) {
+    list()
+  } else {
+    list(public.laboratory = data.frame(laboratory = lab))
+  }
+}
