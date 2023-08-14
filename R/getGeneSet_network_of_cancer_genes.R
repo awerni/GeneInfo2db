7770dbb5bd60ee826fe6760f298003e8384a4ffb
awerni <andreas@wernitznig.com>
2023-08-14 15:05:28 +0200

    take download dir from settings

--- R/getGeneSet_network_of_cancer_genes.R
+++ R/getGeneSet_network_of_cancer_genes.R
@@ -1,27 +1,25 @@
 getGeneSet_network_of_cancer_genes <- function() {
-  
   con <- getPostgresqlConnection()
-  
+
   gene_map <- tbl(con, "normchromentrezgene2ensemblgene") %>%
     dplyr::collect()
-  
+
   RPostgres::dbDisconnect(con)
-  
   # ---------------------------
-  
+
   geneset <- tibble::tribble(
     ~genesetname, ~species,
     "network_of_cancer_genes", "human"
   )
-  
+
   geneassignment <-
-    readr::read_tsv("~/Downloads/geneinfo2db_local_filecache/NCG_cancerdrivers_annotation_supporting_evidence.tsv") |>
+    readr::read_tsv(paste0(getLocalFileRepo(), "/NCG_cancerdrivers_annotation_supporting_evidence.tsv")) |>
     dplyr::select(geneid = entrez) |>
     dplyr::distinct() |>
     dplyr::inner_join(gene_map, by = "geneid") |>
     dplyr::select(-geneid) |>
     dplyr::mutate(genesetname = "network_of_cancer_genes")
-  
+
   list(
     public.geneset = geneset,
     public.geneassignment = geneassignment
