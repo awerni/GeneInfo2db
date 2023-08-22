d91a3c1705a2cd9c27a4dfa0fe17c7c3e0070da4
Andreas Wernitznig <andreas.wernitznig@pierre-fabre.com>
2023-08-22 10:33:32 +0200

    formatting, cosmetics

--- R/getProcessedCopyNumber.R
+++ R/getProcessedCopyNumber.R
@@ -16,7 +16,7 @@ getTissueProjectCopyNumber <- function(project) {
   )
   TCGAbiolinks::GDCdownload(query)
   data_segment <- TCGAbiolinks::GDCprepare(query)
-  data_segment <- data_segment %>% dplyr::filter(substr(Sample,14,15) == "01")
+  data_segment <- data_segment |> dplyr::filter(substr(Sample, 14, 14) == "0")
 
   query <- TCGAbiolinks::GDCquery(
     project = project,
@@ -26,7 +26,6 @@ getTissueProjectCopyNumber <- function(project) {
   )
   TCGAbiolinks::GDCdownload(query)
   data_gene <- TCGAbiolinks::GDCprepare(query)
-
 
   segment_ranges <- with(
     data_segment,
@@ -39,7 +38,7 @@ getTissueProjectCopyNumber <- function(project) {
     )
   )
   gene_ranges <- SummarizedExperiment::rowRanges(data_gene)
-  names(gene_ranges) <- substr(names(gene_ranges),1,15)
+  names(gene_ranges) <- substr(names(gene_ranges), 1, 15)
   gene_ranges <- gene_ranges[!duplicated(names(gene_ranges)),]
   gene_ranges$gene_id <- names(gene_ranges)
 
@@ -62,7 +61,8 @@ getTissueProjectCopyNumber <- function(project) {
       genes_hits <- S4Vectors::queryHits(overlaps)
       segments_hits <- S4Vectors::subjectHits(overlaps)
 
-      segment_dt <- segments %>% as.data.frame %>%
+      segment_dt <- segments |> 
+        as.data.frame() |>
         select(
           Sample,
           log2relativeCopyNumber = Segment_Mean,
@@ -75,8 +75,8 @@ getTissueProjectCopyNumber <- function(project) {
       chunk_dt$ENSG <- genes$gene_id[genes_hits]
       chunk_dt$overlap_width <- width
 
-      chunk_dt <- chunk_dt %>%
-        dplyr::group_by(Sample, ENSG) %>%
+      chunk_dt <- chunk_dt |>
+        dplyr::group_by(Sample, ENSG) |>
         dplyr::summarise(
           log2relativeCopyNumber = weighted.mean(log2relativeCopyNumber, overlap_width),
           .groups = "drop"
@@ -86,22 +86,21 @@ getTissueProjectCopyNumber <- function(project) {
     }
   )
 
-  relative_copy_number <- relative_copy_number %>%
-    dplyr::rename(tissuename = Sample) %>%
+  relative_copy_number <- relative_copy_number |>
+    dplyr::rename(tissuename = Sample) |>
     dplyr::mutate(tissuename = substr(tissuename, 1, 15))
 
   anno_data <- SummarizedExperiment::colData(data_gene)
-  data_gene <- data_gene[,substr(rownames(anno_data), 14, 15) == "01"]
+  data_gene <- data_gene[,substr(rownames(anno_data), 14, 14) == "0"]
 
   copy_raw <- SummarizedExperiment::assay(data_gene, "copy_number")
   rownames(copy_raw) <- substr(rownames(copy_raw),1,15)
   copy_raw <- copy_raw[!duplicated(rownames(copy_raw)), ]
 
-  abs_copy_number <- as.data.frame.table(copy_raw,
-                                         responseName = "totalAbsCopyNumber") %>%
-    dplyr::rename(ENSG = Var1, tissuename = Var2) %>%
+  abs_copy_number <- as.data.frame.table(copy_raw, responseName = "totalAbsCopyNumber") |>
+    dplyr::rename(ENSG = Var1, tissuename = Var2) |>
     mutate(ENSG =  substr(ENSG, 1, 15),
-           tissuename = substr(tissuename, 1, 15)) %>%
+           tissuename = substr(tissuename, 1, 15)) |>
     filter(!is.na(ENSG))
 
   result <- dplyr::full_join(relative_copy_number, abs_copy_number, by = c("tissuename", "ENSG"))
