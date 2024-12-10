261ea605964773e3af0d0303a7074da5d88b3dee
awerni <andreas@wernitznig.com>
2024-12-10 13:52:46 +0100

    inital import

---
+++ data-raw/DB_structure/restoreTissueRNASeqImportBlocker.sql
@@ -0,0 +1,3 @@
+ALTER TABLE tissue.processedrnaseq ADD CONSTRAINT PK_PROCESSEDRNASEQ PRIMARY KEY (ENSG, RNASEQRUNID);
+--CREATE INDEX idx_processedrnaseq  ON tissue.processedrnaseq USING HASH (rnaseqrunid);
+CREATE INDEX idx_processedrnaseq2  ON tissue.processedrnaseq USING HASH (ensg);
